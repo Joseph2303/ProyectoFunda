7f79be342fc501c854b114d210cdbc7bc7177b45
Darwin Javier Silva Gadea <115656897+Infil505@users.noreply.github.com>
2024-06-10 17:40:04 -0600

    logout

--- ProyectoFundamentos/views/logout.js
+++ ProyectoFundamentos/views/logout.js
@@ -1,10 +1,9 @@
-
-document.getElementById('CitasDoctor').addEventListener('click', function(event) {
-    event.preventDefault(); // Prevenir el comportamiento por defecto del enlace
-    logout();
-});
-
 function logout() {
     localStorage.removeItem('token');
     window.location.href = '/ProyectoFundamentos/views/Login/login.html'; 
 }
+
+// Event listener para el icono de "Cerrar Sesión"
+document.getElementById('logoutIcon').addEventListener('click', function() {
+    logout();
+});
